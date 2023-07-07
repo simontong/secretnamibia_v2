2a8849463062ba7407c4b370708294541f7ea90b
Simon Tong <simontong@users.noreply.github.com>
2023-07-07 13:31:50 +1200

    check

--- app/src/app/[...route]/page.tsx
+++ app/src/app/[...route]/page.tsx
@@ -15,7 +15,32 @@ const getRoute = async (props: RouteProps) => {
 	const { params } = props;
 	const route = params.route.join(":");
 
-	const res = await fetch(`${process.env.DIRECTUS_CACHE_URL}/list/test`);
+	// const res = await fetch(`${process.env.DIRECTUS_CACHE_URL}/list/test`);
+	const res = await fetch(`https://httpbin.org/anything`);
+	return res.text();
+};
+
+type RouteProps = {
+	params: {
+		route: string[];
+	};
+	searchParams: URLSearchParams;
+}
+
+export default async function Route(props: RouteProps) {
+	const data = await getRoute(props);
+
+	// if (!data) {
+	// 	return notFound();
+	// }
+
+	return (
+		<>
+			<pre>{JSON.stringify(data, null, 2)}</pre>
+			<br/>
+		</>
+	);
+}
 	return res.text();
 };
 
